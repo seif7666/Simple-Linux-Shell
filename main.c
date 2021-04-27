583afb9088f7b31247cf906951f1d7e04b55b357
seif7666 <seifeldin7666@gmail.com>
2021-04-27 20:18:10 +0200

    Remove useless functions

--- main.c
+++ main.c
@@ -3,18 +3,13 @@
 #include <string.h>
 #include <stdio.h>
 #include<stdlib.h>
+#define SIZE 100 //Max size of input string
 
-#define SIZE 100
 void execute_command(char*,char**,int);
 int get_number_of_spaces(char*);
 void get_args(char*,char**);
 char* exit_str = "exit";
 
-void print(char*input){
-  for(int i=0;i<SIZE;i++)
-    printf(" {%c} ",input[i]);
-  printf("\n");
-}
 int main(){
   char process_name = 1;
   char input[SIZE];
@@ -22,12 +17,12 @@ int main(){
     printf(">>>");
     gets(input);
 
-    int spaces=get_number_of_spaces(input);
+    int spaces=get_number_of_spaces(input); // Needed to get total number of args.
 
     char **args=(char**)calloc(spaces+2,sizeof(char*)); // last character is null.
     args[0] = input;
-    args[spaces+1] = NULL;
-    get_args(input,args);
+    args[spaces+1] = NULL; //Args array must terminate with a null pointer.
+    get_args(input,args); // Now we fill the args array.
     execute_command(input,args,spaces+1);
   }
   return 0;
@@ -60,31 +55,30 @@ void get_args(char* str,char**args){
     }
       ptr++;
   }
-  // printf("Length of args are %d\n",count);
-  // for(int i = 0; i<count;i++)
-  //   printf("%s\n",args[i] );
+
 }
 void execute_command(char* input ,char **args,int length ){
   char wait_arg = 1;
   for(int i = 1;i<length;i++)
-    if(strchr(args[i],'&') != NULL){
+    if(strchr(args[i],'&') != NULL){//Don't wait for child if found.
       args[i] = NULL;
       printf("Don't Wait!\n");
       wait_arg =0;
     }
-  if (strcmp(input,exit_str) == 0){
+  if (strcmp(input,exit_str) == 0){ // Exit the shell
     printf("Exiting ...\n");
     sleep(1.5);
     exit(1);
   }
   int pid=fork();
   if (!pid){
-    sleep(2);
+    sleep(2); // Needed to be sure that parent waits.
     int done=execvp(input,args);
-    printf("Returned value is %d\n",done);
+    if(done ==-1)
+      printf("Error!\n");
   }
   else{
-    signal(SIGCHLD,SIG_IGN);
+    signal(SIGCHLD,SIG_IGN); // To avoid Zombie processes.
     if(wait_arg){
       printf("Waiting for process ...\n");
       // wait(NULL);
